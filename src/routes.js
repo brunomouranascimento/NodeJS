ab8c42737f5233a08d3ae0f1be7a33d77b6e3979
Bruno Nascimento <brunonascimento@MacBook-Pro-de-Bruno.local>
2020-03-06 00:02:27 -0300

    Added controllers

--- src/routes.js
+++ src/routes.js
@@ -1,75 +1,13 @@
-// const fs = require('fs');
-
-// const requestHandler = (req, res) => {
-//   const url = req.url;
-//   const method = req.method;
-//   if (url === '/') {
-//     res.write('<html>');
-//     res.write('<head><title>Enter Message</title></head>');
-//     res.write(
-//       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-//     );
-//     res.write('</html>');
-//     return res.end();
-//   }
-
-//   if (url === '/message' && method === 'POST') {
-//     const body = [];
-//     req.on('data', chunk => {
-//       console.log(chunk);
-//       body.push(chunk);
-//     });
-//     return req.on('end', () => {
-//       const parsedBody = Buffer.concat(body).toString();
-//       const message = parsedBody.split('=')[1];
-//       fs.writeFile('message.txt', message, err => {
-//         res.statusCode = 302;
-//         res.setHeader('Location', '/');
-//         return res.end();
-//       });
-//     });
-//   }
-//   res.setHeader('Content-Type', 'text/html');
-//   res.write('<html>');
-//   res.write('<head><title>My First Page</title><head>');
-//   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-//   res.write('</html>');
-//   res.end();
-// };
-
-// module.exports = requestHandler;
-
-// module.exports = {
-//     handler: requestHandler,
-//     someText: 'Some hard coded text'
-// };
-
-// module.exports.handler = requestHandler;
-// module.exports.someText = 'Some text';
-
-// exports.handler = requestHandler;
-// exports.someText = 'Some hard coded text';
-
 const { Router } = require('express');
-
 const routes = Router();
 
-routes.get('/admin/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
+const productsController = require('./controllers/productsController');
+const errorController = require('./controllers/errorController');
 
-routes.post('/admin/add-product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
+routes.get('/admin/add-product', productsController.getAddProduct);
+routes.post('/admin/add-product', productsController.postAddProduct);
+routes.get('/', productsController.getProducts);
 
-routes.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express</h1>');
-});
-routes.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
-});
+routes.use(errorController.get404);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
 
+const routes = require('./routes');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
 
-app.use(routes);
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
 
 app.listen(3000);
