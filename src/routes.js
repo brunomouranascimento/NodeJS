ddb697d9b50d53b2d540061a170ec3d4349b77fb
Bruno Nascimento <brunonascimento@RBSs-Macbook-Pro-03.local>
2020-03-06 13:22:20 -0300

    MVC added

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,22 @@ const { Router } = require('express');
 const routes = Router();
 
 const productsController = require('./controllers/productsController');
+const shopController = require('./controllers/shopController');
 const errorController = require('./controllers/errorController');
 
 routes.get('/admin/add-product', productsController.getAddProduct);
 routes.post('/admin/add-product', productsController.postAddProduct);
-routes.get('/', productsController.getProducts);
+routes.get('/admin/products', productsController.getProducts);
+
+routes.get('/', shopController.getIndex);
+
+routes.get('/products', shopController.getProducts);
+
+routes.get('/cart', shopController.getCart);
+
+routes.get('/orders', shopController.getOrders);
+
+routes.get('/checkout', shopController.getCheckout);
 
 routes.use(errorController.get404);
 
